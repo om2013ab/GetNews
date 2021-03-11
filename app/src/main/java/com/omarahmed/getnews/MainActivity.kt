d9aa5fd6575fd1c0e0a37f69e1325a49869d980c
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-03-11 11:17:46 +0800

    remove splash screen

--- app/src/main/java/com/omarahmed/getnews/MainActivity.kt
+++ app/src/main/java/com/omarahmed/getnews/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setTheme(R.style.Theme_GetNews)
         setContentView(binding.root)
 
         navController = findNavController(R.id.host_fragment)
