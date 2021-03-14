249580754cd5306282821386229a6eb636d546fa
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-03-14 09:07:47 +0800

    edit explore header

--- app/src/main/java/com/omarahmed/getnews/ui/explore/ExploreAdapter.kt
+++ app/src/main/java/com/omarahmed/getnews/ui/explore/ExploreAdapter.kt
@@ -1,105 +1,50 @@
 package com.omarahmed.getnews.ui.explore
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.omarahmed.getnews.databinding.ExploreHeaderLayoutBinding
 import com.omarahmed.getnews.databinding.ExploreItemsBinding
 import com.omarahmed.getnews.models.Article
-import com.omarahmed.getnews.shared.DataItems
-import com.omarahmed.getnews.shared.DiffCallback
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.lang.ClassCastException
+import com.omarahmed.getnews.shared.ShareClickListener
+import com.omarahmed.getnews.ui.explore.ExploreAdapter.ExploreNewsViewHolder
 
-const val HEADER_TYPE = 0
-const val NEWS_TYPE = 1
+class ExploreAdapter(val clickListener: ShareClickListener) :
+    ListAdapter<Article, ExploreNewsViewHolder>(DiffCallback) {
 
-class ExploreAdapter(val listener: ExploreAdapterInterface) : ListAdapter<DataItems, ViewHolder>(DiffCallback()) {
-    private val adapterScope = CoroutineScope(Dispatchers.Default)
-
-    fun addHeaderAndSubmitList(list: List<Article>?) {
-        adapterScope.launch {
-            val item = when (list) {
-                null -> listOf(DataItems.Header)
-                else -> listOf(DataItems.Header) + list.map {
-                    DataItems.NewsItem(it, itemCount)
-                }
-            }
-            withContext(Dispatchers.Main) {
-                submitList(item)
-            }
+    object DiffCallback : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
         }
 
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
     }
+
     inner class ExploreNewsViewHolder(val binding: ExploreItemsBinding) : ViewHolder(binding.root) {
-        fun bindExplore(dataItems: DataItems.NewsItem) {
-            binding.article = dataItems.article
+        fun bindExplore(article: Article) {
+            binding.article = article
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
-
-        init {
-            binding.ivExploreShare.setOnClickListener {
-                if (adapterPosition != RecyclerView.NO_POSITION){
-                    val currentNews = getItem(adapterPosition) as DataItems.NewsItem
-                    listener.shareNewsLink(currentNews.article)
-                }
-
-            }
-        }
     }
 
-    inner class ExploreHeaderViewHolder(binding: ExploreHeaderLayoutBinding) : ViewHolder(binding.root) {
-        init {
-            listener.setupHeader(binding)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExploreNewsViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return when (viewType) {
-            HEADER_TYPE -> {
-                val headerView = ExploreHeaderLayoutBinding.inflate(layoutInflater, parent, false)
-                ExploreHeaderViewHolder(headerView)
-            }
-            NEWS_TYPE -> {
-                val newsView = ExploreItemsBinding.inflate(layoutInflater, parent, false)
-                ExploreNewsViewHolder(newsView)
-            }
-            else -> throw ClassCastException("Unknown viewType $viewType")
+        val newsView = ExploreItemsBinding.inflate(layoutInflater, parent, false)
+        return ExploreNewsViewHolder(newsView)
+    }
+
+    override fun onBindViewHolder(holder: ExploreNewsViewHolder, position: Int) {
+        val currentNews = getItem(position)
+        holder.bindExplore(currentNews)
+        holder.itemView.setOnClickListener {
+            val action =
+                ExploreFragmentDirections.actionExploreFragmentToDetailsFragment(currentNews)
+            it.findNavController().navigate(action)
         }
     }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        when (holder) {
-            is ExploreNewsViewHolder -> {
-                val currentNews = getItem(position) as DataItems.NewsItem
-                holder.bindExplore(currentNews)
-                holder.itemView.setOnClickListener {
-                   val action = ExploreFragmentDirections.actionExploreFragmentToDetailsFragment(currentNews.article)
-                    it.findNavController().navigate(action)
-                }
-            }
-        }
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return when (getItem(position)) {
-            is DataItems.Header -> HEADER_TYPE
-            is DataItems.NewsItem -> NEWS_TYPE
-        }
-    }
-
-    interface ExploreAdapterInterface {
-        fun setupHeader(headerBinding: ExploreHeaderLayoutBinding)
-        fun shareNewsLink(article: Article)
-    }
-
 }
